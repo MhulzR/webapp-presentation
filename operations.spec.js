8498c1028f72f6f4cf4e44dda35089de4787e2f5
MhulzR <samy.elqannoussi@gmail.com>
2017-11-19 00:33:23 +0100

    fourth test jenkins auto trigger

--- operations.spec.js
+++ operations.spec.js
@@ -2,7 +2,7 @@ const addition = require('./operations').addition;
 
 describe('addition', () => {
   it('should return 5 if I add 2 and 3', () => {
-    const result = addition(2, 4);
-    expect(result).toEqual(6);
+    const result = addition(3, 4);
+    expect(result).toEqual(7);
   });
 });
