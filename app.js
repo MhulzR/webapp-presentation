00fa46d70dc227a8bd2dff987433ef9ac4a886bc
MhulzR <samy.elqannoussi@gmail.com>
2017-11-19 13:26:16 +0100

    new update

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ app.get("/hello/:a/:b", (request, response) => {
   response.send("Hello " + addition(parseInt(request.params.a),parseInt(request.params.b)));
 });
 
-app.listen(3214, () => console.log("Application is runing on port 3214"));
+app.listen(3214, () => console.log("Application is runing on port 3214:"));
--- operations.spec.js
+++ operations.spec.js
@@ -2,7 +2,7 @@ const addition = require('./operations').addition;
 
 describe('addition', () => {
   it('should return 5 if I add 2 and 3', () => {
-    const result = addition(3, 4);
+    const result = addition(3, 6);
     expect(result).toEqual(7);
   });
 });
