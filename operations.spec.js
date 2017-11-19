92c1bbb7a6b89189ab16fd5470524e0d5822593f
MhulzR <samy.elqannoussi@gmail.com>
2017-11-19 13:29:29 +0100

    new update - 2

--- operations.spec.js
+++ operations.spec.js
@@ -1,8 +1,8 @@
 const addition = require('./operations').addition;
 
 describe('addition', () => {
-  it('should return 5 if I add 2 and 3', () => {
-    const result = addition(3, 6);
+  it('should return 7 if I add 2 and 5', () => {
+    const result = addition(2, 5);
     expect(result).toEqual(7);
   });
 });
